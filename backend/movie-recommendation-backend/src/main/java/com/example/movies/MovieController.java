c1d9d3998ed36a8d8e81eab3849edce37aa852ec
noahwie <noahwieschmann2@gmail.com>
2024-10-30 11:14:01 +0100

    Add Rest API

---
+++ backend/movie-recommendation-backend/src/main/java/com/example/movies/MovieController.java
@@ -0,0 +1,26 @@
+package com.example.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Random;
+
+@RestController
+@RequestMapping("/api/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @GetMapping("/recommendation")
+    public Movie getRecommendation() {
+        List<Movie> movies = movieRepository.findAll();
+        if (movies.isEmpty()) {
+            throw new RuntimeException("No movies available!");
+        }
+        return movies.get(new Random().nextInt(movies.size()));
+    }
+}
